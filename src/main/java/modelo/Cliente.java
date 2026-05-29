451aa4603b3fb2b31f2b7cd00be9f41efc863785
andy <andy@Andy>
2026-05-29 11:54:38 -0500

    segundo commit

--- src/main/java/modelo/Cliente.java
+++ src/main/java/modelo/Cliente.java
@@ -1,13 +1,74 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author andy
- */
 public class Cliente {
+    private String RUC;
+    private String razonSocial;
+    private String email;
+    private String contacto;
+    private String telefono;
+    private String clave;
+
+    private Oferta[] ofertas;
+    private int totalOfertas;
+
+    public Cliente(String RUC, String razonSocial, String email, String contacto, String telefono, Rubro rubro) {
+        this.RUC = RUC;
+        this.razonSocial = razonSocial;
+        this.email = email;
+        this.contacto = contacto;
+        this.telefono = telefono;
+        this.clave = clave;
+        this.ofertas = new Oferta[50];
+        this.totalOfertas = 0;
+    }
+
+    public boolean agregarOferta(Oferta o) {
+        if (totalOfertas >= ofertas.length) {
+            return false;
+        }
+        
+        ofertas[totalOfertas++] = o;
+        return true;
+    }
+
+    public boolean eliminarOferta(Oferta o) {
+        for (int i = 0; i < totalOfertas; i++) {
+            if (ofertas[i] == o) {
+                for (int j = i; j < totalOfertas - 1; j++) {
+                    ofertas[j] = ofertas[j + 1];
+                }
+                
+                ofertas[--totalOfertas] = null;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Oferta[] getOfertas() {
+        Oferta[] res = new Oferta[totalOfertas];
+        
+        for (int i = 0; i < totalOfertas; i++) {
+            res[i] = ofertas[i];
+        }
+        
+        return res;
+    }
+
+    public String getRUC() { 
+        return RUC; 
+    }
     
+    public String getRazonSocial() { 
+        return razonSocial; 
+    }
+    
+    public String getClave() { 
+        return clave; 
+    }
+
+    @Override
+    public String toString() { 
+        return razonSocial + " | RUC: " + RUC; 
+    }
 }
